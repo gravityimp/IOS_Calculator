7724f8e8a9d7eb6eaac787044fb1e0cdfc1e2a44
Yurii Tyshchenko <orty222@gmail.com>
2023-10-03 17:05:57 +0200

    Initial Commit

---
+++ IOS_Calculator/IOS_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_CalculatorApp.swift
+//  IOS_Calculator
+//
+//  Created by student on 03/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct IOS_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
